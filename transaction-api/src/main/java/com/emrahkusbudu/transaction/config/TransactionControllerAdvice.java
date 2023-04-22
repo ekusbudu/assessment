d58cbb84994a42b93089f1190dddcd63c9504c01
ekusbudu <ekusbudu@gmail.com>
2023-04-22 14:22:48 +0100

    handleCustomerNotFoundException added

--- transaction-api/src/main/java/com/emrahkusbudu/transaction/config/TransactionControllerAdvice.java
+++ transaction-api/src/main/java/com/emrahkusbudu/transaction/config/TransactionControllerAdvice.java
@@ -2,6 +2,7 @@ package com.emrahkusbudu.transaction.config;
 
 import com.emrahkusbudu.transaction.dto.ErrorResponse;
 import com.emrahkusbudu.transaction.exception.TransactionCreationException;
+import com.emrahkusbudu.transaction.exception.TransactionNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,12 @@ public class TransactionControllerAdvice extends ResponseEntityExceptionHandler 
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(TransactionNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleCustomerNotFoundException(TransactionNotFoundException ex) {
+        logError("Transaction not found exception", ex);
+        ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
     private void logError(String logLabel, Exception ex) {
         log.error(logLabel + ": {}", ex.getMessage());
     }
